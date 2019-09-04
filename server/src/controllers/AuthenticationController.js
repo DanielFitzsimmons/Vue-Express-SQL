f5d2e6a691ffcb26bc680e42071e722bba7aefbd
Daniel Fitzsimmons <daniel_fit@e46941-imac.local>
2019-09-05 09:59:20 +1000

    send data to sqlite

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,16 @@
+const {User} = require('../models')
+
 module.exports = {
 
-    register (req, res) {
-        res.send({message: `Hello ${req.body.email}! You have been registered`})
+    async register (req, res) {
+        try{
+            const user = await User.create(req.body)
+            res.send(user.toJSON())
+        }catch(err) {
+            res.status(400).send({
+                error: 'This email account is already in use'
+            })
+        }
     }
 
 }
