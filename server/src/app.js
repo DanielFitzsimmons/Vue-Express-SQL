383bb4e7ac41147bab054da2066eda9925e944f7
Daniel Fitzsimmons <daniel_fit@e46941-imac.local>
2019-09-04 16:34:47 +1000

    sql lite server

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config.js')
 
 const app = express();
 
@@ -16,4 +18,8 @@ app.post('/register', (req, res) =>{
     res.send({message: `Hello ${req.body.email}! You have been registered`})
 })
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync().then(()=> {
+    app.listen(process.env.PORT || config.port)
+    console.log(`Server started on port ${config.port}`)
+})
+
---
+++ server/src/config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+    //process.env.DB_NAME is a environmental variable if that is not set then use 'vueproject'
+    database: process.env.DB_NAME || 'vueproject',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    options: {
+    //dialect - refers to the type of database you are connecting to
+    dialect: process.env.DIALECT || 'sqlite',
+    host: process.env.HOST || 'localhost',
+    storage: './vueproject.sqlite',
+    }
+    }
+    }
