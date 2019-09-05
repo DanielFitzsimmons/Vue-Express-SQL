9f323529a556815541eb3f76f7c183ee2a5bf7aa
Daniel Fitzsimmons <daniel_fit@e46941-imac.local>
2019-09-05 10:47:31 +1000

    vue register error msg

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -3,6 +3,9 @@
 <h1>Register</h1>
 <input type="email" name="email" placeholder="email" v-model="email">
 <input type="password" name="password" placeholder="password" v-model="password">
+
+<div v-html="error" class="error"></div>
+
 <button @click="register">Register</button>
 </div>
 </template>
@@ -12,18 +15,26 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
     name: 'Register',
     data () {
-    return {
-    email: 'test@test.com',
-    password: 'password'
-}
+        return {
+        email: 'test@test.com',
+        password: 'password',
+        error: null
+    }
 },
 methods: {
     async register () {
-        const responce = await AuthenticationService.register({
-        email: "test@test",
-        password: this.password
-        })
-        console.log(responce.data)
+
+        try 
+        {
+            await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+        } catch (error) 
+        {
+            this.error = error.response.data.error
+        }
+
     }
 }
 }
@@ -31,4 +42,7 @@ methods: {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .error {
+        color: red;
+    }
 </style>
